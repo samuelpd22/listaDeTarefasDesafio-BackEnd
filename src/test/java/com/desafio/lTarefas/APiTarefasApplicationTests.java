636c9c2ced7ff479d709c0571629b92ab1f3695f
samuelpd22 <samuel050101@hotmail.com>
2024-10-30 18:35:59 -0300

    init

---
+++ src/test/java/com/desafio/lTarefas/APiTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.lTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APiTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
