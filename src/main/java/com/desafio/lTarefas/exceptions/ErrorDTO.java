3aa89a6f9ce2f70548736a7b7e6d72b155fa3c67
samuelpd22 <samuel050101@hotmail.com>
2024-10-31 19:53:55 -0300

    feat : add tratamento de exception

---
+++ src/main/java/com/desafio/lTarefas/exceptions/ErrorDTO.java
@@ -0,0 +1,28 @@
+package com.desafio.lTarefas.exceptions;
+
+public class ErrorDTO {
+    private int status;
+    private String message;
+
+    public ErrorDTO(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    // Getters e Setters
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/desafio/lTarefas/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.desafio.lTarefas.exceptions;
+
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler({ConstraintViolationException.class,})
+    public ResponseEntity<ErrorDTO> handleConstraintViolation(ConstraintViolationException ex) {
+        StringBuilder messageBuilder = new StringBuilder("Erros de validação: ");
+
+        ex.getConstraintViolations().forEach(violation -> {
+            messageBuilder.append(violation.getMessage()).append(" ");
+        });
+
+        ErrorDTO erro = new ErrorDTO(HttpStatus.BAD_REQUEST.value(), messageBuilder.toString().trim());
+        return new ResponseEntity<>(erro, HttpStatus.BAD_REQUEST);
+    }
+}
