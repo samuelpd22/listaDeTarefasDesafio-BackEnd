a8e8e97c88d34e68132d0b8a25d3b36aca279f48
samuelpd22 <samuel050101@hotmail.com>
2024-11-05 20:56:06 -0300

    feat : novos metodos subir/descer objeto na tb

--- src/main/java/com/desafio/lTarefas/controller/TarefaController.java
+++ src/main/java/com/desafio/lTarefas/controller/TarefaController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200/")
 @RestController
 @RequestMapping ("/tarefas")
 public class TarefaController {
