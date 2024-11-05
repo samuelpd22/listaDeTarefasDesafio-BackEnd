72552963d720cdf88b1a9ea00b089f4b242163c0
samuelpd22 <samuel050101@hotmail.com>
2024-11-05 20:26:59 -0300

    feat : criando novos meios de acesso ao db

--- src/main/java/com/desafio/lTarefas/repository/TarefaRepository.java
+++ src/main/java/com/desafio/lTarefas/repository/TarefaRepository.java
@@ -3,7 +3,9 @@ package com.desafio.lTarefas.repository;
 import com.desafio.lTarefas.entity.TarefaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TarefaRepository extends JpaRepository<TarefaEntity, Long> {
@@ -14,4 +16,16 @@ public interface TarefaRepository extends JpaRepository<TarefaEntity, Long> {
     @Query("SELECT MAX(t.ordemApresentacao) FROM TarefaEntity t")
     Integer findMaxOrdemApresentacao();
 
+    Optional<TarefaEntity> findByNomeTarefa(String nomeTarefa);
+
+
+    TarefaEntity findByOrdemApresentacao(Integer ordemApresentacao);
+
+    @Query("SELECT t FROM TarefaEntity t WHERE LOWER(TRIM(t.nomeTarefa)) LIKE LOWER(CONCAT('%', :nomeTarefa, '%'))")
+    List<TarefaEntity> findByNomeTarefaIgnoreSpacesAndCase(@Param("nomeTarefa") String nomeTarefa);
 }
+
+
+
+
+
